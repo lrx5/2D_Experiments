3ba97cee1557843f4b8bcff8f48d034fb16e94e7
lrx5 <143709045+lrx5@users.noreply.github.com>
2024-10-10 01:12:17 +0800

    Update cannon_bullet_one.gd

--- Scripts/TowerDefense/cannon_bullet_one.gd
+++ Scripts/TowerDefense/cannon_bullet_one.gd
@@ -2,13 +2,15 @@ extends Node2D
 
 var bullet_speed = 1500.0
 var velocity: Vector2 = Vector2.ZERO
-@export var lifetime: float = 2.0
+@export var lifetime: float = 1.0
 @onready var lifetime_timer: Timer = $LifetimeTimer
 
 func _ready() -> void:
+	lifetime_timer.wait_time = lifetime
 	lifetime_timer.start()
 	
 func _process(delta: float) -> void:
+	lifetime_timer.wait_time = lifetime
 	position += velocity * bullet_speed * delta
 	
 func set_velocity(new_velocity: Vector2) -> void:
@@ -16,8 +18,6 @@ func set_velocity(new_velocity: Vector2) -> void:
 
 func _on_lifetime_timer_timeout() -> void:
 	queue_free()
-
-
 func _on_bullet_area_body_entered(body: Node2D) -> void:
 	body.health -= 1
 	queue_free()
